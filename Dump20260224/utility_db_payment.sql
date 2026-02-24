72063f01bb7f3695d8f554373ff26cd5346e1305
Anjana2552 <anjanaviswanath9910@gmail.com>
2026-02-24 09:52:58 +0530

    sql tables 24/02/26

---
+++ Dump20260224/utility_db_payment.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: utility_db
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payment` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `amount` decimal(12,2) NOT NULL,
+  `payment_date` datetime(6) NOT NULL,
+  `payment_method` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bill_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `payment_bill_id_2d285b3f_fk_utility_bill_id` (`bill_id`),
+  KEY `payment_payment_d8e241_idx` (`payment_method`),
+  KEY `payment_status_1c1ab2_idx` (`status`),
+  CONSTRAINT `payment_bill_id_2d285b3f_fk_utility_bill_id` FOREIGN KEY (`bill_id`) REFERENCES `utility_bill` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment`
+--
+
+LOCK TABLES `payment` WRITE;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+INSERT INTO `payment` VALUES (1,1896.00,'2026-01-22 10:57:05.524680','online','approved',1),(2,1896.00,'2026-01-24 05:28:39.572655','online','rejected',1),(3,1896.00,'2026-01-28 05:57:06.853149','online','rejected',1),(4,1896.00,'2026-01-28 06:01:15.435274','online','rejected',1),(5,1896.00,'2026-01-28 06:24:14.563762','online','approved',1),(6,800.00,'2026-01-29 04:57:24.670472','online','approved',2),(7,799.00,'2026-01-31 12:35:33.661269','online','rejected',4),(8,799.00,'2026-02-18 15:58:39.686360','debit_card','rejected',4),(9,799.00,'2026-02-20 12:34:10.132022','bank_transfer','approved',4),(10,800.00,'2026-02-22 13:03:36.008866','credit_card','approved',5),(11,1600.00,'2026-02-22 17:46:05.380959','credit_card','approved',6),(12,800.00,'2026-02-23 07:30:56.964659','credit_card','rejected',16);
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-24  9:48:19
